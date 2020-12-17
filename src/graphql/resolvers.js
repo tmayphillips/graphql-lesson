ab10527b31f7e2a321a4438131ab471bffc793b8
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-12-16 21:00:28 -0600

    added resolvers.js, mutations on the client, adding items with Apollo

---
+++ src/graphql/resolvers.js
@@ -0,0 +1,57 @@
+import { gql } from 'apollo-boost'
+
+import { addItemToCart } from './cart.utils'
+
+export const typeDefs = gql`
+    extend type Item {
+        quantity: Int
+    }
+
+    extend type Mutation {
+        ToggleCartHidden: Boolean!
+        AddItemToCart(item: Item!): [Item]!
+    }
+`;
+
+const GET_CART_HIDDEN = gql`
+    {
+        cartHideen @client
+    }
+`;
+
+const GET_CART_ITEMS = gql`
+ {
+     cartItems @client
+ }
+`
+
+export const resolvers = {
+    Mutation: {
+        toggleCartHidden: (_root, _args, { cache }) => {
+            const { cartHidden } = chache.readQuery({
+                query: GET_CART_HIDDEN
+            })
+            cache.writeQuery({
+                query: GET_CART_HIDDEN,
+                data: { cartHIdden: !cartHidden }
+            })
+
+            return !cartHidden;
+        },
+
+        addItemToCart: (_root, { item }, { cache }) => {
+            const { cartItem } = cache.readQuery({
+                query: GET_CART_ITEMS
+            })
+
+            const newCartItems = addItemToCart(cartItems, item)
+
+            cache.writeQuery({
+                query: GET_CART_ITEMS,
+                data: { cartItems: newCartItems }
+            })
+
+            return newCartItems
+        }
+    }
+}
